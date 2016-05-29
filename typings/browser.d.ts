fe58c2231b421c93973f5434c6613aea18fd9c8e
vsternbach <vlad@logz.io>
2016-05-29 15:51:27 +0300

    fixed typings problem

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/angular-ui-router/index.d.ts" />
-/// <reference path="browser/ambient/angular/index.d.ts" />
-/// <reference path="browser/ambient/jquery/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/angular-ui-router/index.d.ts" />
-/// <reference path="main/ambient/angular/index.d.ts" />
-/// <reference path="main/ambient/jquery/index.d.ts" />
